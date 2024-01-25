41499efdec216aa4727fec69054c8c74f772f070
yusuf <yusufgad24@gmail.com>
2024-01-25 10:25:00 +0300

    The Wild Oasis App

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Wil Oasis Notes
+
+I don't know if after will change it, but it's a bad practice to use styled components inside the file component. Need to be in their own files, better organization, to locate and update and most important for the scalability. And personally I always add .style.jsx to be help the maintainability.
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 248px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 8px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button>check in</Button>
+      <Button>check out</Button>
+
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  );
+}
+
+export default App;
