3728c0c50416b45f2fc25da3b8a123e6fcc1c00a
yusuf <yusufgad24@gmail.com>
2024-03-06 09:56:57 +0300

    Building the dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>statistics</div>
+      <div>todays activity</div>
+      <div>chart stay durations</div>
+      <div>chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
